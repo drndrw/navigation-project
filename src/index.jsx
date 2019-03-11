3e8b09ddbdfaea8d120b1ca775ee388fc1d71c7f
Andrew DaRe <>
2019-03-10 20:45:14 -0700

    Built default render component

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {HashRouter} from "react-router-dom";
+
+// Component imports
+import Navigation from './components/navigation';
+
+ReactDOM.render((
+  <div className="app">
+    <Navigation />
+  </div>
+), document.body);
