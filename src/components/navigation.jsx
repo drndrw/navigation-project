ca2a3cf9acd11f3823b699a8665e5c9687b40f84
Andrew DaRe <>
2019-03-10 22:37:30 -0700

    Defined state for line x values, left offset value and updateLine function

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,15 +2,39 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x1: 0,
+      x2: 0,
+      leftOffset: 0
+    };
+    this.updateLine = this.updateLine.bind(this);
+  }
+
+  updateLine(e, animate=true) {
+    let location = document.getElementById(e.target.id).getBoundingClientRect();
+    this.setState({
+      x1: location.left,
+      x2: location.right
+    }, () => {
+      console.log(this.state.x1);
+      console.log(this.state.x2);
+    });
+  }
 
   render () {
     return (
       <div className="navigation">
         <ul>
           {this.props.routes.cities.map((city) => (
-            <li><NavLink exact to={city.section} className="nav-link" activeClassName="nav-active">{city.label}</NavLink></li>
+            <li><NavLink exact to={city.section} id={city.section} onClick={this.updateLine} className="nav-link" activeClassName="nav-active">{city.label}</NavLink></li>
           ))}
         </ul>
+        <svg height="1" width="100%">
+          <line id="nav-line" x1="0" y1="0" x2="100%" y2="0" />
+          <line id="link-select" x1="0" y1="0" x2="0" y2="0" />
+        </svg>
       </div>
     );
   }
