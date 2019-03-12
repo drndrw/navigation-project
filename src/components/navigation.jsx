14bad63f81bfa87282921e8053e769881397f473
Andrew DaRe <>
2019-03-11 23:13:20 -0700

    Defined x1, x2, leftOffset and activeSection as state

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,20 +2,43 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 class Navigation extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    activeSection: null,
+    x1: 0,
+    x2: 0,
+    leftOffset: 0
+  };
+  this.updateLine = this.updateLine.bind(this);
+  this.resizeLine = this.resizeLine.bind(this);
+}
 
   componentDidMount() {
     this.updateLine(this.props.activeRoute, true);
+    window.addEventListener('resize', this.resizeLine());
   }
 
   updateLine(element, initial=false) {
+    let leftOffset = document.getElementById('nav-line').getBoundingClientRect().left;
     if (initial) {
-      var location = document.getElementById(element).getBoundingClientRect();
+      let location = document.getElementById(element).getBoundingClientRect();
+      this.setState({activeSection: element, x1: location.left - leftOffset, x2: location.right - leftOffset, leftOffset: leftOffset});
     } else {
-      var location = document.getElementById(element.target.id).getBoundingClientRect();
+      let location = document.getElementById(element.target.id).getBoundingClientRect();
+      this.setState({activeSection: element, x1: location.left - leftOffset, x2: location.right - leftOffset, leftOffset: leftOffset});
     }
-    let leftOffset = document.getElementById('nav-line').getBoundingClientRect().left
+  }
+
+  moveLine(activeSection) {
+    let location = document.getElementById(activeSection).getBoundingClientRect();
+    let leftOffset = document.getElementById('nav-line').getBoundingClientRect().left;
     document.getElementById('link-select').setAttribute('x1', location.left - leftOffset);
     document.getElementById('link-select').setAttribute('x2', location.right - leftOffset);
+  }
+
+  resizeLine() {
+    console.log('resizing');
   }
 
   render () {
@@ -28,7 +51,7 @@ class Navigation extends React.Component {
         </ul>
         <svg height="1" width="100%">
           <line id="nav-line" x1="0" y1="0" x2="100%" y2="0" />
-          <line id="link-select" x1="0" y1="0" x2="0" y2="0" />
+          <line id="link-select" x1={this.state.x1} y1="0" x2={this.state.x2} y2="0" />
         </svg>
       </div>
     );
